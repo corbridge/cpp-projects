99c5cdd1c97368e179dcc1a70c03e9e7bf07e146
Cornelio Posadas P <posadascornelio38@gmail.com>
2024-07-14 17:31:50 -0600

    add gitignore

---
+++ snake_game/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    std::cout<<"Hola"<<std::endl;
+    Sleep(2000);
+
+    bool end = false;
+    while(!end)
+    {
+        std::cout<<"H"<<std::endl;
+
+        
+    }
+}
