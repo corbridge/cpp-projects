027f58cf0db9e3941ef7190e898d205ddc1a21ec
Cornelio Posadas P <posadascornelio38@gmail.com>
2024-08-22 20:01:52 -0600

    replace cls

--- snake_game/src/main.cpp
+++ snake_game/src/main.cpp
@@ -4,14 +4,18 @@
 
 int main()
 {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD cursorPos = {0, 0};
+
     Board tablero;
 
     tablero.displayBoard();
     for(int i = 0; i < 10; i++)
     {
         tablero.updateBoard(i + 1, 9);
-        system("cls");
+        SetConsoleCursorPosition(hConsole, cursorPos);
         tablero.displayBoard();
+        Sleep(20);
 
     }
 }
